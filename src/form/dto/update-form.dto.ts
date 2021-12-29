918c0304d7ffaf83b65e222c87b1372fad25b5d9
KieenVux <40339436+KieenVux@users.noreply.github.com>
2021-12-29 18:45:21 +0700

    Fix Swagger

--- src/form/dto/update-form.dto.ts
+++ src/form/dto/update-form.dto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { IsEnum } from 'class-validator';
+import { IsIn } from 'class-validator';
 import { FormStatus } from '../entities/form.entity';
 import { CreateFormDto } from './create-form.dto';
 
+const { ACCEPTED, PENDING, REJECTED } = FormStatus;
 export class UpdateFormDto extends PartialType(CreateFormDto) {
-  @IsEnum(FormStatus)
+  @IsIn([ACCEPTED, PENDING, REJECTED])
   @ApiProperty({
     enum: FormStatus,
+    default: PENDING,
   })
   status: FormStatus;
 }
